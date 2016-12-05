b5c0f24dab3a720cca1040a96eee777eb3a4d43c
xingmo.liu <linkivan3588@gmail.com>
2016-12-05 10:18:25 -0500

    new sql

--- script/mysql.sql
+++ script/mysql.sql
@@ -33,12 +33,14 @@ CREATE TABLE `assignment` (
   `weight` varchar(45) DEFAULT NULL,
   `course_id` int(11) NOT NULL,
   `file_id` int(11) DEFAULT NULL,
+  `submit_by` varchar(45) NOT NULL,
+  `description` varchar(400) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_assignment_course1_idx` (`course_id`),
   KEY `fk_assignment_file1_idx` (`file_id`),
   CONSTRAINT `fk_assignment_course1` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_assignment_file1` FOREIGN KEY (`file_id`) REFERENCES `file` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,6 +49,7 @@ CREATE TABLE `assignment` (
 
 LOCK TABLES `assignment` WRITE;
 /*!40000 ALTER TABLE `assignment` DISABLE KEYS */;
+INSERT INTO `assignment` VALUES (1,'assignment1','2016-12-17','public','10',NULL,1,NULL,'',NULL),(2,'assignment1','2016-12-17','public','10','20%',1,NULL,'',NULL),(3,'assignment1','2016-12-17','public','10','20%',1,NULL,'',NULL),(4,'assignment1','2016-12-10','Published','10','20%',1,NULL,'File',NULL),(6,'assignment1','2016-12-10','Published','50','20%',12,NULL,'File','asaa'),(7,'assignment2','2016-12-08','Unpublished','10','30%',12,NULL,'Email','asa'),(8,'assignment3','2016-12-03','Published','100','5%',12,NULL,'Email','qsaas');
 /*!40000 ALTER TABLE `assignment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,13 +99,10 @@ CREATE TABLE `course` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `room` varchar(45) DEFAULT NULL,
   `time` varchar(45) DEFAULT NULL,
-  `instructor_id` int(11) DEFAULT NULL,
   `code` varchar(45) NOT NULL,
   `semester` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_course_user1_idx` (`instructor_id`),
-  CONSTRAINT `fk_course_user1` FOREIGN KEY (`instructor_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -111,7 +111,7 @@ CREATE TABLE `course` (
 
 LOCK TABLES `course` WRITE;
 /*!40000 ALTER TABLE `course` DISABLE KEYS */;
-INSERT INTO `course` VALUES (1,'123','14:00-15:00',NULL,'enpm611','fall2016'),(2,'123','14:00-15:00',NULL,'enpm612','fall2016'),(3,'123z','14:00-15:00',NULL,'enpm614','fall2016');
+INSERT INTO `course` VALUES (1,'123e','14:00-15:00e','enpm611e','Summer2017'),(3,'123z','14:00-15:00','enpm614','Fall2016'),(4,'45321','45312','123','Fall2016'),(5,'5','5','5','Fall2016'),(6,'2','2','2','2'),(8,'66','66','667','Fall2016'),(9,'1212','1212','12','Select'),(10,'1212','1212','12','Select'),(12,'s4','14:00-15:00','enpm613','Fall2016'),(13,'541','4141','5','Winter2016');
 /*!40000 ALTER TABLE `course` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -183,8 +183,9 @@ CREATE TABLE `user` (
   `role` varchar(45) NOT NULL,
   `account_non_locked` tinyint(1) NOT NULL DEFAULT '1',
   `login_attempts` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -193,7 +194,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'student1','1518596cc02033138901826200c26c60',1,'ROLE_STU',1,0);
+INSERT INTO `user` VALUES (1,'student1','1518596cc02033138901826200c26c60',1,'ROLE_STU',1,0),(2,'admin','b594510740d2ac4261c1b2fe87850d08',1,'ROLE_ADMIN',1,0),(3,'instructor1','e820b3ccd0518ef85581594be163fb92',1,'ROLE_INSTR',1,0),(4,'instructor2','d1d664b63ca62c765efc46705bbd4eb9',1,'ROLE_INSTR',1,0),(5,'student2','a47dba34dd6dc11e1a62f3d48c585bd7',1,'ROLE_STU',1,0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -222,6 +223,7 @@ CREATE TABLE `users_courses` (
 
 LOCK TABLES `users_courses` WRITE;
 /*!40000 ALTER TABLE `users_courses` DISABLE KEYS */;
+INSERT INTO `users_courses` VALUES (1,1,0),(1,3,0),(1,12,0),(3,1,0),(3,3,0),(3,12,0);
 /*!40000 ALTER TABLE `users_courses` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -234,4 +236,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-28 22:27:56
+-- Dump completed on 2016-12-05 10:17:32
